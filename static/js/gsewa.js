8ab4de7851efcd3b27a977d598bf2eb0fd0ee4fc
RazinDangol <dangolrajin@gmail.com>
2016-01-07 15:54:55 +0545

    Updated frontend to view total amount and # of transation

--- static/js/gsewa.js
+++ static/js/gsewa.js
@@ -1,4 +1,15 @@
+var totals=[0,0];
 $(document).ready(function(){
-	"use strict";
-	$('#table2 tbody').append('<tr><td>Ncell</td><td>Postpaid</td><td>1234567</td></tr>');
+    
+        var $dataRows=$("#total tr:not('.totalData')");
+            $dataRows.each(function() {
+                $(this).find('.rowData').each(function(i){        
+                    totals[i]+=parseInt( $(this).html());
+                });
+            });
+            console.log(totals);
+	$('#total td.totalData').each(function(i){
+    console.log(this);
+    $(this).html(totals[i]);
+});
 	});
