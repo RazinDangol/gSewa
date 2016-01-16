7442e9b2d3d8b5a4dc5e5b43d5303f59e6bc9c7b
Razin Dangol <dangolrajin@gmail.com>
2016-01-16 22:35:55 +0545

    Update frontend upload design

---
+++ static/js/upload.js
@@ -0,0 +1,32 @@
+
+var status_url;
+$('#upload').dmUploader({
+	url: '/process',
+    dataType: 'json',
+    onComplete: function(){
+    	console.log("Upload complete");
+     },
+	 
+    onUploadSuccess: function(id,data){
+    	status_url = data.location;
+       	console.log(status_url);
+        $('div#upload').html("<div id='processing'><h2>Processing... Please wait</h2><img src='../static/loader.gif' style='width:70px;height:70px;'></div>");
+		//Polling the server if the processing task is completed or not	
+     	Poll();
+    	function Poll(){
+    		$.getJSON(status_url, function(data) {
+         	// process results here
+        		if(data.state ==="SUCCESS"){
+          			console.log('Success');
+          			window.location.href = 'http://127.0.0.1:5000/payment/all';
+        		}
+        		else{
+          			console.log('pending');
+        			setTimeout(Poll,1000);
+      			}
+
+    		});
+		}
+	}
+});
+     
