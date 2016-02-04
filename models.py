57d933491f69f2438103fe0e3c9332ea2061ba17
Razin Dangol <dangolrajin@gmail.com>
2016-02-04 09:51:04 +0545

    Added transfer table

--- models.py
+++ models.py
@@ -43,6 +43,27 @@ class Cashback(db.Model):
     def __repr__(self):
         return '{}'.format(self.service_provider)
 
+class Transfer(db.Model):
+    __tablename__ = "transfer"
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    service_provider=db.Column(db.String)
+    service = db.Column(db.Text)
+    service_name = db.Column(db.String)
+    service_type = db.Column(db.String)
+    amount = db.Column(db.Integer)
+    status = db.Column(db.String)
+    time = db.Column(db.String)
+    def __init__(self,service_provider,service,service_name,service_type,amount,status,time):
+        self.service_provider = service_provider
+        self.service = service
+        self.service_name = service_name
+        self.service_type = service_type
+        self.amount = amount
+        self.status = status
+        self.time = time
+    def __repr__(self):
+        return '{}'.format(self.service_provider)
+
 class Info(db.Model):
     __tablename__ = 'info'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
